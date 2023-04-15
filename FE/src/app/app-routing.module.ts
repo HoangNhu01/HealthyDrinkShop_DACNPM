fccfa22f0337d294d7c8f779553c8b9299a0e4c2
Tan Kieu <tankieu7c@gmail.com>
2023-04-15 15:38:37 +0700

    fake data các màn hình :
    1.home
    2.danh sách sản phẩm
    3.chi tiết sản phẩm

--- FE/src/app/app-routing.module.ts
+++ FE/src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {AboutUsComponent} from "./about-us/about-us.component";
+import {DetailProductComponent} from "./detail-product/detail-product.component";
+import {AllProductComponent} from "./all-product/all-product.component";
 
 const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about-us', component: AboutUsComponent },
+  { path: 'detail-product', component: DetailProductComponent },
+  { path: 'all-product', component: AllProductComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
+  declarations: [
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
